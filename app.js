d3c5a69f28b33dc778f8c487e2fd77ee49b169c1
Konstantin Kuts <karzuan@gmail.com>
2020-04-28 14:26:03 +0800

    completed log infunctionality

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const selenium = require("selenium-webdriver");
-const driver = selenium.Builder().forBrowser("chrome").build();
+const driver = new selenium.Builder().forBrowser("chrome").build();
 const url = "https://www.linkedin.com/login";
 driver.get(url);
+const By = selenium.By;
+const userName = driver.findElement(By.xpath("/html/body/div/main/div/form/div/input"));
+userName.sendKeys("username");
+const passWord = driver.findElement(By.xpath("//*[@id='password']"));
+passWord.sendKeys("12345");
+const loginButton = driver.findElement(By.xpath('//*[@id="app__container"]/main/div[2]/form/div[3]/button'));
+loginButton.click();
