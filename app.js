8f819b80867728cde5582000c9094be50b294347
Konstantin Kuts <karzuan@gmail.com>
2020-04-29 11:10:07 +0800

    add prompt module with selecting of choices.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const selenium = require("selenium-webdriver");
-const driver = new selenium.Builder().forBrowser("chrome").build();
+//const driver = new selenium.Builder().forBrowser("chrome").build();
+const prompts = require('prompts');
+const questions = require('./questions');
+
+
+/*
 const url = "https://www.linkedin.com/login";
 driver.get(url);
 const By = selenium.By;
@@ -9,3 +14,11 @@ const passWord = driver.findElement(By.xpath("//*[@id='password']"));
 passWord.sendKeys("12345");
 const loginButton = driver.findElement(By.xpath('//*[@id="app__container"]/main/div[2]/form/div[3]/button'));
 loginButton.click();
+*/
+
+  (async () => {
+    const response = await prompts(questions);
+   
+    // => response => { username, age, about }
+    //console.log('your login is ' + response.username + " and pssw is " + response.password );
+  })();
